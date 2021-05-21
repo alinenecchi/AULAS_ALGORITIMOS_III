361d40170ad1db6597ae5913e310f799dbfc0b21
aline dias necchi ribeiro <alinenecchi@gmail.com>
2021-05-20 22:27:44 -0300

    Jogo de corrida

---
+++ aula09/jogo_parte01.py
@@ -0,0 +1,38 @@
+import pygame
+pygame.init()
+x = 400
+y = 300 
+velocidade = 10
+
+fundo = pygame.image.load('pista.png')
+carro = pygame.image.load('carromenor.png')
+
+
+
+janela = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Criando jogo com Python")
+
+janela_aberta = True
+while janela_aberta :
+    pygame.time.delay(50)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            janela_aberta = False
+
+    comandos = pygame.key.get_pressed()
+    if comandos[pygame.K_UP]:
+        y -= velocidade
+    if comandos[pygame.K_DOWN]:
+        y += velocidade
+    if comandos[pygame.K_RIGHT]:
+        x -= velocidade
+    if comandos[pygame.K_LEFT]:
+            x += velocidade    
+
+  
+    janela.blit(fundo,(0,0))
+    janela.blit(carro,(x,y))
+    pygame.display.update()
+pygame.quit()
+
+
